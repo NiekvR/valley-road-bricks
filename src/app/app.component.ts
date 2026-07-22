344b92ed7343e28d0b6a008c970d00288ed4eb5a
NiekvR <niekvrabenswaaij@gmail.com>
2026-07-22 21:47:34 +0200

    Integrate all components into main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProcessComponent } from './components/process/process.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    HeroComponent,
+    ProductsComponent,
+    ProcessComponent,
+    NewsletterComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'valley-road-bricks';
+  title = 'Valley Road Bricks';
+  cartCount: number = 0;
 }
